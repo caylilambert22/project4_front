15350d14ffe6e682812cbe53e7ddb8df53b3ad43
caylilambert22 <caylilambert@gmail.com>
2025-05-09 10:44:32 -0400

    trying to add new route

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forum" element={<Forum />} />
